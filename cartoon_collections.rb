89b8d04d4a446350dfc515a7ec766a46af5b2079
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-03-28 22:40:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,12 +10,10 @@ array.collect do |array|
 end
   end
 
-def long_planeteer_calls(array)
-if array.count <= 3
-  return false
-else
-  return true
-end
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
